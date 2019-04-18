86e156bbac2a7d16590390654680be0c772f61b8
dodo88c <dodo88c@gmail.com>
2019-04-18 02:53:42 +0200

    Add files via upload

---
+++ fonction.h
@@ -0,0 +1,104 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+void init(int pionW[20], int pionB[20], int kingW[20], int kingB[20]){ //initialise le plateau
+	
+    for(int i=1;i<21;i++){
+    
+        pionW[i]=i;
+        pionB[i]=51-i;
+
+        kingW[i]=0;
+        kingB[i]=0;
+    }
+}
+
+
+
+
+char Joueur(int pionW[20], int pionB[20] , int caseD){ //Donne l'équipe
+	for(int i=1;i<21;i++){
+		if(caseD==pionB[i]){	
+			return B; //équipe noire
+		}		
+		if(caseD==pionW[i]){
+	
+			return W; //équipe blanche 
+		}
+	
+	}
+}
+
+
+
+
+void VerifP(int pionW[20], int pionB[20] , int caseD){ //vérifie la case la plus petite(gauche)
+	Joueur(pionW[],pionB[],caseD);
+
+	int a=0;
+	
+	if(Joueur=='B'){
+		if (caseD==6||caseD==16||caseD==26||caseD==36||caseD==46||caseD==47||caseD==48||caseD==49||caseD==50){
+			a=1;
+		}
+		for(int i=1;i<21;i++){
+			if(caseD+4==pionW[i] || caseD+4==pionB[i] || a=1){
+				return False; //case ocupée
+			}
+			else
+				return True; //retourne case libre
+			
+		}
+
+	}
+	if(Joueur=='W'){
+		if (caseD==5||caseD==15||caseD==25||caseD==35||caseD==45||caseD==1||caseD==2||caseD==3||caseD==4){
+			a=1;
+		}
+		for(int i=1;i<21;i++){
+			if(caseD+4==pionW[i] || caseD+4==pionB[i] || a=1){
+				return False; //case ocupée
+			}
+			else
+				return True; //retourne case libre
+			
+		}
+
+	}
+	
+}
+
+
+
+
+void VerifG(int pionW[20], int pionB[20] , int caseD){ //vérifie la case la plus grande(droite)
+	if
+		return True; //retourne case libre
+	esle	
+		return False; //case ocupée 
+}
+
+
+
+
+
+
+
+
+
+
+int depl(int pionW[20], int pionB[20] , int caseD){
+	rand	
+
+	return caseA; //retourne case d'arrivé
+}
+
+
+
+
+
---
+++ remplissage_base.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include fonction.h;
+#include <stdlib.h>
+#include <time.h>
+
+
+
+
+int main(){
+
+    int a=0; i=0, j=0, k=0, l=0;
+    
+    printf("entrer le degré de la base à generer");                               // demande la valeur de la table à remplir
+    scanf(a);
+
+    while(a==0 || a==1 || a>20){
+        printf("entrer le degré de la base à generer, entre 2 et 20");
+        scanf(a);                                                                 //                                       exemple: 5
+    }
+        
+
+
+    // la merde commence
+
+// declaration de toute les configurations d'equipes:
+
+
+    for(int k=0, k < a, k++){                   // reine W de 0 à  a-1                                                      exemple: 1
+        for(int l=0, l < (a-k), l++{            // reine B de 0 à  a-k-1                                                    exemple: 2
+
+            int i=1, j=1;
+            if(k>0){ int i=0;}
+            if(l>0){ int i=0;}
+
+
+            for( i; i <= (a-k-l); i++){         // pion W  de 0 si reine W >0, sinon de 1  à a-k-l                           exemple: 2
+            int j= (a-k-l)-i+1 ;                // pion B  de 0 si reine B >0, sinon de 1  à a-k-l+1 (inverse de i)          exemple: 0
+
+            
+
+	            int pionW[i]; //initialisation des pions blanc
+	            int pionB[j]; //noir
+
+	            int kingW[k]; //initialisation des reines blanche
+	            int kingB[l]; //noir
+
+            
+	        
+// ici toute les configurations d'equipes sont faite.
+// declaration de toute les positions de pions:
+
+                
+                
+
+                for( int za=1, za<51, za++){     
+    
+                    for( int zb=1, zb<51, zb++){
+
+          
+                        for( int zc=1, zc<51, zc++){ 
+                            for( int zd=1, zd<51, zd++){ 
+
+                                if( za=zb || za=zc || za==zd || zb==zc || zb==zd || zc==zd ) {  NULL }      // passe si deux pions sont sur la meme case
+                                else{
+
+
+                                    if(k > 0){  } 
+
+                                    if(l > 0){  } 
+
+
+
+
+
+
+
+
+
+                                }       // fermeture de "passe si deux pions sont sur la meme case"
+
+
+
+                            }       // fermeture des positions de pions
+                        }
+                    }
+                }
+
+
+
+
+            }       // fermeture des configurations d'equipes
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
