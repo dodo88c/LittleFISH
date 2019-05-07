bf380bfa420e5991b0b4f571f04e3e4abb54c294
PcO <dodo88c@gmail.com>
2019-05-07 16:29:38 +0200

    initialisation partie
    666

--- code/IA_LittleFISH.c
+++ code/IA_LittleFISH.c
@@ -2,7 +2,7 @@
 #include fonction.h;
 #include <stdlib.h>
 #include <time.h>
-
+#include <interface.h>
 
 
 
@@ -13,9 +13,17 @@ int main(){
 
 	int kingW[20]; //initialisation des reines blanche
 	int kingB[20]; //noir
+	int init,noir=1,blanc=50;	
+	for(init=1;init<21;init++){ //initialisation de la partie
+		kingW[init]=0;
+		kingB[init]=0;
+		pionW[init]=blanc;
+		pionB[init]=noir;
+		blanc--;
+		noir++;
+	}
+	affichage(pionW[],pionB[],kingW[],kingB[]); //affichage grille
 
 
 }
 
-
-testetzt
